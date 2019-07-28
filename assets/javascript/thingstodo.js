47fbf87886b9e908cf0a05aa375d0ec757500f7d
mp02 <molly_patterson@yahoo.com>
2019-07-28 16:31:53 -0500

    updates at the end of class

--- assets/javascript/thingstodo.js
+++ assets/javascript/thingstodo.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
 
+    var queryURL = "https://developers.zomato.com/api/v2.1/&api_key=0a2997fca00a53b5e2ced3c26766ef4d";
+    var wiki = "https://en.wikipedia.org/w/api.php"
 
-    $.ajax({
+console.log (queryURL);
+console.log(wiki);
+   
+   $.ajax({
         url: queryURL,
         method: "GET"
     })
