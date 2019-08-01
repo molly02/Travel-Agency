e296efae64b9b769cec205d70c1737beed0840fd
Hari <augten95@gmail.com>
2019-08-01 12:40:28 -0500

    modified queryURL to use user input in the api call

--- assets/javascript/flights.js
+++ assets/javascript/flights.js
@@ -11,11 +11,11 @@ $(document).ready(function() {
     var numAdults = $("#numAdults").val().trim();
     var numChildren = $("#numChildren").val().trim();
     var numInfants = $("#numInfants").val().trim();
-    var queryURL = "https://developer.goibibo.com/api/search/?app_id=c1ea2a16&app_key=ba8d21d387b423781cf4a234ff1c731c&format=json&source=ORD&destination=YYZ&dateofdeparture=20190730&dateofarrival=20190730&seatingclass=E&adults=1&children=0&infants=0&counter=0";
+    var queryURL = "https://developer.goibibo.com/api/search/?app_id=c1ea2a16&app_key=ba8d21d387b423781cf4a234ff1c731c&format=json&source="+origin+"&destination="+destination+"&dateofdeparture="+newDepartureDate+"&dateofarrival="+newArrivalDate+"&seatingclass="+classOfTravel+"&adults="+numAdults+"&children="+numChildren+"&infants="+numInfants+"&counter="+0;
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        // $().text(JSON.stringify(response));
+        $(".subheader-flights").text(JSON.stringify(response));
     });
 });
