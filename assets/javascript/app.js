072c7db5c86c252af8beae2a0f5e8c6e3bb3d192
Shynggys Makhanbetov <shynggysmakhanbetov@shynggyss-mbp.attlocal.net>
2019-08-01 11:17:42 -0500

    made some changes on my branch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,22 +30,12 @@ $(document).ready(function () {
 		b.addClass('movie'); // add a class
 		b.attr('data-movie', movie); // add a data-attribute
 		b.text(movie); // make button text
-		// movie.push(a);
 		$('#movie-buttons').append(b);
-		// movies.push(movie);
-		// $('#movie-buttons').append(movie);
-		// renderButtons();
-		// return;
 	});
-
-
 	// Getting gifs from api... onto html
 	$("#movie-buttons").on("click", function (event) {
 		console.log(event)
 		var movie = $(event.target).attr("data-movie");
-		// var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-		// 	movie + "&api_key=VUyrDCYI9BaLHto1DiocGfqlbwnrEHny&limit=2"
-		//     // VUyrDCYI9BaLHto1DiocGfqlbwnrEHny
 		var queryURL = "https://api.unsplash.com/search/photos?query=" +
 			movie
 			+ "&per_page=5&client_id=a442736c796a1b94c0186b0f5ba64322591bfc5b8d98816bb6f7d2c63706d6aa"
@@ -57,25 +47,13 @@ $(document).ready(function () {
 			$("#movies").empty();
 			for (var i = 0; i < results.length; i++) {
 				var movieDiv = $('<div>');
-				// var p = $("<p>").text("Rating: " + results[i].rating);
-
-				// var 
-				// var link = $('<a href="https://www.google.com/maps/search/"' + movies + '"/@42.0843388,-87.9365323,10z/data=!3m1!4b1">').text(link);
-				// // link.attr("href", 'https://www.google.com');
-
 				var link = $("<a>");
 				link.attr("href", "https://www.google.com/maps/search/" + movie + "/@42.0846639,-87.935583,10z");
-				link.attr("title", "Google.com");
-				link.text("Google");
+				link.attr("title", "For Hotels link it");
+				link.text("For Hotels link it");
 				link.addClass("link");
-
 				var movieImg = $("<img>");
 				movieImg.attr("src", results[i].urls.regular);
-				// movieImg.attr("data-still", results[i].images.original_still.url);
-				// movieImg.attr("data-animate", results[i].images.original.url);
-				// movieImg.attr("data-state", "still");
-				// movieImg.attr("class", "gif");
-				// movieDiv.append(p);
 				movieDiv.append(link);
 				movieDiv.append(movieImg);
 
